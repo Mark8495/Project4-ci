262cc157fa03dc03db8f55892dee9cfd38ada50e
Mark8495 <markbyrne8495@gmail.com>
2022-09-16 20:22:04 +0000

    created recipe and comment model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    # preptime = models.ForeignKey(
+    #     Preptime,
+    #     on_delete=models.CASCADE,
+    #     related_name="recipe_posts"
+    # )
+    # mealtime = models.ForeignKey(
+    #     Mealtime,
+    #     on_delete=models.CASCADE,
+    #     related_name="recipe_posts"
+    # )
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="recipe_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    updated_on = models.DateTimeField(auto_now=True)
+    serving_size = models.CharField(max_length=30, default=0)
+    ingredients = models.TextField(blank=True)
+    instructions = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, 
+        related_name='recipe_likes', 
+        blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
